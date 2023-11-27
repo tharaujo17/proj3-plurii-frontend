397f15dbd6c40a805ebce8170b4c1e836189224d
Mariane Fontes <mbsf@cesar.school>
2023-11-27 12:17:53 -0300

    Footer update

---
+++ pluriiv2/src/Shared/Components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container} from '../Footer/styled' 
+
+const Footer: React.FC = () => {
+    return (
+      <Container>
+        
+      </Container>
+    );
+  };
+  
+  export default Footer;
+  
