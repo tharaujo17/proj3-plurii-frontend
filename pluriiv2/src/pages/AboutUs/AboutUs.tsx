bfc6f3f5def0e7e3564e63dacb594471352e8ae2
Mariane Fontes <mbsf@cesar.school>
2023-11-28 19:33:10 -0300

    criação de pastas e arquivos para 'Sobre' e 'Contato'

---
+++ pluriiv2/src/pages/AboutUs/AboutUs.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const AboutUS: React.FC = () => {
+  return <></>;
+};
+
+export default AboutUS;
---
+++ pluriiv2/src/pages/Contact/Contact.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Contact: React.FC = () => {
+  return <></>;
+};
+
+export default Contact;
