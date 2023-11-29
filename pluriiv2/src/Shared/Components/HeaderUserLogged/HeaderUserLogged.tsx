afeebeee589f411fe741b48d553fe234d2082061
Francisco Antonio da Luz Filho <falf@cesar.school>
2023-11-29 20:36:21 -0300

    Update HeaderUserLogged.tsx

--- pluriiv2/src/Shared/Components/HeaderUserLogged/HeaderUserLogged.tsx
+++ pluriiv2/src/Shared/Components/HeaderUserLogged/HeaderUserLogged.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import{ 
     Container,
@@ -18,6 +18,44 @@ import MenuSanduba from '../../../assets/header-menu-sandwich.svg';
 import ProfilePic from '../../../assets/profile_pic.png';
 
 const HeaderUserLogged: React.FC = () => {
+    const [userData, setUserData] = useState<any>(null);
+
+    useEffect(() => {
+      // Função para obter dados do usuário
+      const fetchUserData = async () => {
+        try {
+          // Recupere o token de acesso do localStorage
+          const accessToken = localStorage.getItem('access_token');
+  
+          // Verifique se o token está presente
+          if (accessToken) {
+            // Faça a solicitação autenticada
+            const response = await fetch('https://plurii.up.railway.app/users/email/', {
+              method: 'GET',
+              headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${accessToken}`, // Adicione o token ao cabeçalho de autorização
+              },
+            });
+  
+            if (!response.ok) {
+              // Lidere com erros de solicitação, como token expirado, etc.
+              console.error('Erro na solicitação:', response.status, response.statusText);
+            } else {
+              // Processar a resposta bem-sucedida
+              const data = await response.json();
+              setUserData(data); // Atualize o estado com os dados do usuário
+            }
+          } else {
+            console.log('Token de acesso ausente. Redirecionando para a página de login.');
+          }
+        } catch (error) {
+          console.error('Erro ao obter dados do usuário:', error);
+        }
+      };
+  
+      fetchUserData();
+    }, []);
     return (
         <Container>
               <Wrapper>
@@ -28,7 +66,7 @@ const HeaderUserLogged: React.FC = () => {
                 <UserInfoWrapper>
                     <UserImage src= {ProfilePic}/>
                 <UserInfoDetails>
-            <UserName>Carla Silva</UserName>
+            <UserName>{userData.username}</UserName>
             <UserRA>RA: 2023010895</UserRA>
         </UserInfoDetails>
       </UserInfoWrapper>
